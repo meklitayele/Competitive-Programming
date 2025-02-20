af34545fe048ada8a42a5339ffa4323734901141
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-02-20 11:21:42 +0300

    Time: 39 ms (91.97%), Space: 19.7 MB (54.88%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -7,25 +7,22 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         fast , slow = head , head
-        while fast and fast.next :
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-            if slow==fast:
-                slow = head 
+            if slow == fast:
+                slow = head
                 while slow != fast:
                     slow = slow.next
                     fast = fast.next
+
                 return slow
-                
+
         return None
-                
 
                 
-        #         ans = "tail connects to node index " + pos
-        #         return ans
-        
-        # ans = "no cycle"
+     
         
         
     
