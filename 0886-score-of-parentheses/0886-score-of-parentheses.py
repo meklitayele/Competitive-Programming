64d07b28c72bd15eb96e4e6d5c7d1c8732475fba
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-07 22:14:11 +0300

    Time: 0 ms (100%), Space: 17.9 MB (7.68%) - LeetHub

--- 0886-score-of-parentheses/0886-score-of-parentheses.py
+++ 0886-score-of-parentheses/0886-score-of-parentheses.py
@@ -2,35 +2,22 @@ class Solution:
     def scoreOfParentheses(self, s: str) -> int:
         stack = []
         n = len(s)
-       
         for i in range(n):
             if s[i] == '(':
-                stack.append('(')
-                print(stack)
+                stack.append(s[i])
             else:
                 val = stack.pop()
-                if  val == '(' :
+                if val == '(':
                     if stack and str(stack[-1]).isdigit():
                         stack[-1] += 1
                     else:
-                        stack.append(1)
-            
-                    print(stack)
+                        stack.append(1)     
                 elif str(val).isdigit():
                     val2 = stack.pop()
                     if stack and str(stack[-1]).isdigit():
                         stack[-1] += 2*val
                     else:
                         stack.append(2*val)
-                    print(stack)
-        # print(stack)
-        ans = stack[0]
-        return ans
+        return stack[0]
 
-
-                
-
-
-
-        
         
