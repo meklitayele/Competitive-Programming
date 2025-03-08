bfec14e11fae62bbf127b6aa41ce9807b8f1863d
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-08 23:07:11 +0300

    Time: 0 ms (100%), Space: 19.1 MB (11.45%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dictionary:
+                return [i,dictionary[target-nums[i]]]
+            dictionary[nums[i]] = i
+        return []
+
+
+        
+        
+        
