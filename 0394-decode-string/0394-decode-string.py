8087455e9e1c7b831de699ca679d23d8bb888cdb
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-11 18:27:28 +0300

    Time: 0 ms (100%), Space: 18 MB (13.37%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -7,16 +7,17 @@ class Solution:
                     digit = ""
                     while s[i] != '[':
                         digit += s[i]
-                        i+=1
-                    res,index = recu(i+1)
+                        i +=1 
+                    res , index = recu(i+1)
                     i = index
                     ans.append(res * int(digit))
                 elif s[i] == ']':
-                    return ''.join(ans),i+1
+                    return ''.join(ans) , i+1
                 else:
                     ans.append(s[i])
                     i+=1
-            return ''.join(ans),i
+            return ''.join(ans) , i
+            
         result , index = recu(0)
         return result
         
