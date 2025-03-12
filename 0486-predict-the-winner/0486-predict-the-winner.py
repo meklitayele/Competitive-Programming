8aa313ceeeb6e75595a038f5f00d4a7f9fdeea9c
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-12 13:55:37 +0300

    Time: 4192 ms (6.4%), Space: 17.6 MB (91.04%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,40 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def play(nums,turn,i,j):
+            #best case
+            if i > j:
+                return 0
+            if (turn):
+                return max(nums[i] + play(nums,False,i+1,j),nums[j] + play(nums,False,i,j-1))
+            else:
+                return min(play(nums,True,i+1,j), play(nums,True,i,j-1))
+        
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+        
+        player1 = play(nums,True,0,len(nums)-1)
+        player2 = total - player1
+
+        return player1 >= player2
+
+
+
+            
+
+
+        #get the total sum to get the sum for player 2
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+
+        
+ 
+
+            
+        
+        
+                
+
+
+        
