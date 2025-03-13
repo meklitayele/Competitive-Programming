1804ba656672a466b6b92a4de565721d0268d2aa
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-13 22:51:29 +0300

    Time: 0 ms (100%), Space: 17.9 MB (38.1%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
@@ -1,20 +1,20 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        def calc(n,k):
-            if n == 1:
+        def calc(n , k):
+            length = 2 ** (n-1)
+            mid = length / 2
+
+            if n == 1 :
                 return 0
 
-            length =  2 ** (n-1)
-            mid = length // 2
-
-            #first half is the direct copy
             if k <= mid:
                 return calc(n-1,k)
             else:
                 return 1 - calc(n-1,k-mid)
-
         ans = calc(n,k)
         return ans
+        
+
 
         
          
