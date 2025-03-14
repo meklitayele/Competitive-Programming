7563e3608c181e0b958446de78246d8588dd7f0e
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-14 13:37:12 +0300

    Time: 0 ms (100%), Space: 17.9 MB (54.58%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,14 +6,10 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def calc(n1,n2):
+        def go(n1,n2):
             if not n1 and not n2:
                 return True
             if not n1 or not n2:
                 return False
-
-            return n1.val == n2.val and calc(n1.left,n2.right) and calc(n1.right,n2.left)
-        ans = calc(root.left , root.right)
-        return ans
-
-        
+            return n1.val == n2.val and go(n1.left,n2.right) and go(n1.right,n2.left)
+        return go(root.left , root.right)
