e84764407940ab48c890e25de0d8240d05447cb3
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-14 16:32:35 +0300

    Time: 0 ms (100%), Space: 17.7 MB (61.7%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
@@ -1,11 +1,10 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        def calc(n , k):
+        def calc(n,k):
+            if n == 1:
+                return 0
             length = 2 ** (n-1)
             mid = length / 2
-
-            if n == 1 :
-                return 0
 
             if k <= mid:
                 return calc(n-1,k)
@@ -13,6 +12,7 @@ class Solution:
                 return 1 - calc(n-1,k-mid)
         ans = calc(n,k)
         return ans
+        
         
 
 
