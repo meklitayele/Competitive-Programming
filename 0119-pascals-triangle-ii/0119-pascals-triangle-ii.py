3496d9e7b2e64f9b15c7ce4d8d608682fdf672bd
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-14 10:19:58 +0300

    Time: 0 ms (100%), Space: 18.3 MB (11.95%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,13 +1,20 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         @cache
-        def get(row,col):
-            if col == 0 or row == col:
+        def calc(row,col):
+            if row == col:
                 return 1
-            return get(row-1,col-1) + get(row-1,col)
-        ans = [get(rowIndex,col) for col in range(rowIndex+1)]
+            if col == 0:
+                return 1
+            else:
+                return calc(row-1,col-1) + calc(row-1,col)
+        ans = [calc(rowIndex,col) for col in range(rowIndex+1)]
         return ans
-       
+
+            
+           
+
+        
 
     
         
