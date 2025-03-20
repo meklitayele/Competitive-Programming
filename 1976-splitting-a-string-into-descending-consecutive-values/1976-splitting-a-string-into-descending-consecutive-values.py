811d6cd48e8cceccc394aff4dafbd83253f2611e
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-20 14:23:24 +0300

    Time: 3 ms (69.97%), Space: 17.9 MB (23.89%) - LeetHub

--- 1976-splitting-a-string-into-descending-consecutive-values/1976-splitting-a-string-into-descending-consecutive-values.py
+++ 1976-splitting-a-string-into-descending-consecutive-values/1976-splitting-a-string-into-descending-consecutive-values.py
@@ -4,19 +4,17 @@ class Solution:
         def backtrack(index,prev):
             if index == n:
                 return True
-                
             for j in range(index,n):
                 val = int(s[index:j+1])
-                if val + 1 ==  prev and backtrack(j+1,val):
+                if val + 1 == prev and backtrack(j+1,val):
                     return True
             return False
-
+        
         for i in range(n-1):
             val = int(s[:i+1])
-            if backtrack(i+1,val) :
+            if backtrack(i+1,val):
                 return True
-
         return False
 
 
-        
+
