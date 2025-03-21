9423df1d75fa6b86204a57e53f7d58a2c2fb6aa0
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-21 10:53:22 +0300

    Time: 172 ms (5.06%), Space: 17.8 MB (57.15%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -3,27 +3,17 @@ class Solution:
         ans = []
         n = len(candidates)
         candidates.sort()
-        store = []
-        def backtrack(index,s):
-            if s == target:
-                ans.append(store[::] )
-                return
-            if s>target:
+        def backtrack(index,store):
+            if sum(store) == target:
+                ans.append(store[:])
                 return 
-            
             for j in range(index,n):
-                if s < target:
+                if sum(store) < target:
                     if j > index and candidates[j] == candidates[j-1]:
                         continue
                     store.append(candidates[j])
-                    s+=candidates[j]
-                   
-                    backtrack(j+1,s)
-                    
-                    s -= store.pop()
-                    
-        backtrack(0,0)
+                    backtrack(j+1,store)
+                    store.pop()
+
+        backtrack(0,[])
         return ans
-
-
-        
