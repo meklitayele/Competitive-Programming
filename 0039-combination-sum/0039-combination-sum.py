b4a6b72bbb7aeefbcb1ef8cb8f437f8bd72d09c7
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-21 10:45:31 +0300

    Time: 40 ms (9.54%), Space: 17.9 MB (61.84%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -4,9 +4,9 @@ class Solution:
         n = len(candidates)
         def backtrack(index,store):
             if sum(store) == target:
-                ans.append(store[:])   
+                ans.append(store[:])
                 return 
-            for j in range(index ,n):
+            for j in range(index,n):
                 if sum(store) < target:
                     store.append(candidates[j])
                     backtrack(j,store)
@@ -14,27 +14,7 @@ class Solution:
         backtrack(0,[])
         return ans
 
-                
-                
 
-            # for num in candidates:
-            #     total += num
-            #     if total == target:
-            #         print(store)
-            #         ans.append(store[:])  
-            #         return
-            #     if total > target:
-            #         continue
-                
-            #     backtrack(store,total)
-            #     store.pop()
-
-        # for num in candidates:
-        #     if num == target:
-        #         ans.append(num)
-        #     else:
-        #         backtrack([],0)
-        return ans
 
 
 
