9cc0cd9f0ed4dc5fb41972cf5513ecd36b190c5a
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-21 11:30:44 +0300

    Time: 7 ms (7.62%), Space: 18.1 MB (14.84%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -3,19 +3,15 @@ class Solution:
         ans = []
         n = len(nums)
         nums.sort()
-        def backtrack(index,store): 
-            if len(store) <= n and store not in ans:
+        def backtrack(index,store):
+            if store not in ans:
                 ans.append(store[:])
+
             for j in range(index,n):
-                if j < index and nums[j] == nums[j-1]:
-                    continue
                 store.append(nums[j])
                 backtrack(j+1,store)
                 store.pop()
         backtrack(0,[])
         return ans
 
-         
 
-
-        
