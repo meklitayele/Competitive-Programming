6b796c87ae86516819c50589e2bfbf9510aed4db
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-26 11:16:41 +0300

    Time: 0 ms (100%), Space: 18.1 MB (68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -12,11 +12,7 @@ class Solution:
                     left = mid + 1
             else:
                 if target > nums[mid] and target <= nums[right]:
-                    left = mid + 1
+                    left =  mid + 1
                 else:
                     right = mid - 1
-        return - 1
-
-
-
-        
+        return -1
