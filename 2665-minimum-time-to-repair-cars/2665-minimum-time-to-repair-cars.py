7ca2b958811dcbaf3d699f2e87ef4f266ce737e9
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-03-27 08:25:40 +0300

    Time: 1268 ms (11.35%), Space: 22 MB (51.48%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -8,15 +8,15 @@ class Solution:
                 return True
             else:
                 return False
-         
-        left , right = 0 , min(ranks) * cars * cars
-        res = right
-        while left <= right:
-            mid = (left + right) // 2  
-            if valid(mid):
-                right = mid - 1
-                res = min(res,mid)
-            else:
-                left = mid + 1
-        return res
 
+        left , right = 0 , pow(10,18)
+        while left + 1 < right:
+            mid = (left + right)//2
+            if valid(mid):
+                right = mid
+            else:
+                left = mid
+        return right
+
+        
+    
