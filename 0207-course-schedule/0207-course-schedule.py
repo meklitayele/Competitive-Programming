cf0896c4a62d372adfc2417c904f21777f41e012
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-09 14:37:18 +0300

    Time: 0 ms (100%), Space: 19.3 MB (40.94%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        n = numCourses
+        color = ['white' for _ in range(n)]
+
+        store = defaultdict(list)
+        for i , j in prerequisites:
+            store[i].append(j)
+
+        def dfs(node):
+            if color[node] == 'grey':
+                return False
+            if color[node] == 'black':
+                return True
+
+            color[node] = 'grey'
+            for neighbour in store[node]:
+                if not dfs(neighbour):
+                    return False
+            color[node] = 'black'
+            return True
+        
+        for i in range(n):
+            if not dfs(i):
+                return False
+        return True
+            
+
+        
+        
+        
+
+
