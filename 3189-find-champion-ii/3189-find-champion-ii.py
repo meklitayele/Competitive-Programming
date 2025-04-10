dce1079156e0b0dcb5ee95e7612b3d09377152c8
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-10 09:30:37 +0300

    Time: 129 ms (5.67%), Space: 19 MB (11.82%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.py
+++ 3189-find-champion-ii/3189-find-champion-ii.py
@@ -4,27 +4,22 @@ class Solution:
         for i , j in edges:
             store[i].append(j)
         
-        visited = set()
         def dfs(node,visited):
-            for n in store[node]:
-                if n not in visited:
-                    visited.add(n)
-                    dfs(n,visited)
+            for ne in store[node]:
+                if ne not in visited:
+                    visited.add(ne)
+                    dfs(ne,visited)
+               
 
-       
 
+        visited = set()
         for i in range(n):
-            if i not in visited:
-                visited.add(i)
-                dfs(i,visited)
-                if len(visited) == n:
-                    return i
-            visited = set()   
+            visited.add(i)
+            dfs(i,visited)
+            if len(visited) == n:
+                return i
+            visited = set()
 
-       
-        return -1
-
-                
+        return - 1
 
             
-                    
