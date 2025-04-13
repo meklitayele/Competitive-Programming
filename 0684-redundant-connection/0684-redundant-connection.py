688825ed405202014300a4eb739f19157e69c2fe
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-13 07:06:21 +0300

    Time: 11 ms (15.47%), Space: 18.6 MB (15.94%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -3,22 +3,19 @@ class Solution:
         def dfs(node,parent,visited):
             if node == parent:
                 return True
+
             visited.add(node)
             for n in store[node]:
                 if n not in visited:
-                    visited.add(n)
                     if dfs(n,parent,visited):
                         return True
             return False
-
         store = defaultdict(list)
-        
-        for a , b in edges:
+        for a ,b in edges:
             visited = set()
             if dfs(a,b,visited):
                 return [a,b]
             store[a].append(b)
             store[b].append(a)
-        
         return []
-        
+            
