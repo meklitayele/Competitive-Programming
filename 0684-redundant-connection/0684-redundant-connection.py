91f4ed9de43c369d27b94b79b8ff5455eee7c2c2
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-13 07:01:14 +0300

    Time: 11 ms (15.47%), Space: 18.5 MB (26.1%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        def dfs(node,parent,visited):
+            if node == parent:
+                return True
+            visited.add(node)
+            for n in store[node]:
+                if n not in visited:
+                    visited.add(n)
+                    if dfs(n,parent,visited):
+                        return True
+            return False
+
+        store = defaultdict(list)
+        
+        for a , b in edges:
+            visited = set()
+            if dfs(a,b,visited):
+                return [a,b]
+            store[a].append(b)
+            store[b].append(a)
+        
+        return []
+        
