49a47a792c8ec6f85c1724a0dd650661f979d65a
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-21 09:12:59 +0300

    Time: 24 ms (43.29%), Space: 17.9 MB (57.05%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -1,35 +1,32 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
+        def getCoord(curr):
+            row , col = (curr - 1)//n , (curr-1) % n
+            if row % 2 == 1:
+                col = n - 1 - col
+            return n - 1 - row , col
+        
         deq = deque()
         deq.append(1)
         visited = set()
         visited.add(1)
-        move = 0
-         
+        moves = 0
 
-        def cord(curr):
-            r , c = (curr - 1) // n , (curr - 1) % n
-            if r % 2 == 1:
-                c = n - 1 - c
-            return n - 1 - r , c
-        
         while deq:
             for _ in range(len(deq)):
                 curr = deq.popleft()
-                if curr == n**2:
-                    return move
-
-                for nxt in range(curr+1,min(curr+7,n*n+1)):
-                    r , c = cord(nxt)
+                if curr == n*n:
+                    return moves
+                for nxt in range(curr + 1, min(curr + 7, n*n+1)):
+                    r , c = getCoord(nxt)
                     if board[r][c] != -1:
                         nxt = board[r][c]
                     if nxt not in visited:
                         visited.add(nxt)
                         deq.append(nxt)
-            move += 1
+            moves += 1
         return -1
-
 
 
             
