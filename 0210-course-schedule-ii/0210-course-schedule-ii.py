913fb149ca6a59a819a2f6df91a51e6270e3a463
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-24 10:00:35 +0300

    Time: 4 ms (50.2%), Space: 19.2 MB (48.87%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,32 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         store = defaultdict(list)
-        color = ['white'] * numCourses
-        for a , b in prerequisites:
-            store[b].append(a)
-        
-        flag = False
-        res  = []
-        def bfs(node):
-            nonlocal flag
-            if color[node] == 'grey':
-                flag = True
-                return 
-            if color[node] == 'black':
-                return 
-            color[node] = 'grey'
-
-            for n in store[node]:
-                bfs(n)
-                if flag:
-                    return 
-            color[node] = 'black'
-            res.append(node)
-
-        
+        indegree = [0] * numCourses
+        for x , y in prerequisites:
+            store[y].append(x)
+            indegree[x] += 1
+        deq = deque()
         for n in range(numCourses):
-            if color[n] == 'white':
-                bfs(n)
-                if flag:
-                    return []
-        return res[::-1]
+            if indegree[n] == 0:
+                deq.append(n)
+        
+        res = []
+        while deq:
+            node = deq.popleft()
+            res.append(node)
+            for n in store[node]:
+                indegree[n] -= 1
+                if indegree[n] == 0:
+                    deq.append(n)
+        return res if len(res) == numCourses else []
+
+
+        
+        
