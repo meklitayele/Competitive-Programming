26ccecb40417d03bb4ac6dd27a5f7d7a732114b7
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-24 08:02:07 +0300

    Time: 13 ms (71.32%), Space: 24.5 MB (81.49%) - LeetHub

--- 0881-loud-and-rich/0881-loud-and-rich.py
+++ 0881-loud-and-rich/0881-loud-and-rich.py
@@ -1,15 +1,19 @@
 class Solution:
     def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
-        rich = defaultdict(list)
         n = len(quiet)
+        rich = defaultdict(list)
         indegree = [0] * n
+
         for x , y in richer:
             rich[x].append(y)
             indegree[y] += 1
         
-        
+        deq = deque()
         res = [i for i in range(n)]
-        deq = deque([i for i in range(n) if indegree[i]== 0])
+        for i in range(n):
+            if indegree[i] == 0:
+                deq.append(i)
+
         while deq:
             node = deq.popleft()
             for n in rich[node]:
