08f0cc4345479d19ea488529eed19f3eafd400f4
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-24 08:20:56 +0300

    Time: 77 ms (63.01%), Space: 35.6 MB (61.01%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,28 +1,32 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         store = defaultdict(list)
-        for a , b in edges:
-            store[b].append(a)
-
+        indegree = [0] * n
+        for x , y in edges:
+            store[x].append(y)
+            indegree[y] += 1
         print(store)
-        res = []
-        parents = []
-        def dfs(node,visited):
-            nonlocal parents 
-            for n in store[node]:
-                if n not in visited and n not in parents:
-                    parents.append(n)
-                    visited.add(n)
-                    dfs(n,visited)
-            return sorted(parents)
 
+        deq = deque()
         for i in range(n):
-            if i in store.keys():
-                ans = dfs(i,set())
-                res.append(ans)
-                parents = []
-            else:
-                res.append([])
+            if indegree[i] == 0:
+                deq.append(i)
+        
+        res =[[] for _ in range(n)]
+        while deq:
+            node = deq.popleft()
+            for n in store[node]:
+                res[n].extend(res[node])
+                res[n].append(node)
+                res[n] = list(sorted(set(res[n])))
+                indegree[n] -= 1
+                if indegree[n] == 0:
+                    deq.append(n)
         return res
 
-            
+
+
+        
+
+
+                
