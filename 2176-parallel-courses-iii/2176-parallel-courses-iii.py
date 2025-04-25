eb788b93c068cc3630aacec838d2b639849856de
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-25 18:43:30 +0300

    Time: 127 ms (84.26%), Space: 42.4 MB (80.15%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.py
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.py
@@ -2,24 +2,29 @@ class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
         store = defaultdict(list)
         indegree = [0] * (n+1)
+
         for x , y in relations:
             store[x].append(y)
             indegree[y] += 1
-
-        deq= deque()
+        
+        deq = deque()
         t = [0] * (n+1)
         for i in range(1,n+1):
             if indegree[i] == 0:
                 deq.append(i)
                 t[i] = time[i-1]
-       
-       
+
+        
         while deq:
             node = deq.popleft()
             for v in store[node]:
+                t[v] = max(time[v-1]+t[node],t[v])
                 indegree[v] -= 1
-                t[v] = max(t[v],time[v-1] + t[node])
                 if indegree[v] == 0:
                     deq.append(v)
             
+        print(t)
         return max(t)
+
+
+
