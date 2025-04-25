12ad8e922ea59e283d77c6efc1fd5d5228f4c4ed
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-25 09:31:57 +0300

    Time: 3 ms (82.3%), Space: 17.9 MB (27.74%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -7,23 +7,29 @@ class Solution:
                 if grid[i][j] == 2:
                     deq.append((i,j,0))
 
-        directions = [(1,0),(-1,0),(0,-1),(0,1)]
-        def inbound(r,c):
-            return 0 <= r < n and 0 <= c < m
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        def inbound(x,y):
+            return 0<=x<n and 0<=y<m
 
         total = 0
         while deq:
-            i , j , time = deq.popleft()
+            x , y , time = deq.popleft()
             for dx , dy in directions:
-                newx , newy = dx + i , dy + j
-                if inbound(newx,newy) and grid[newx][newy] == 1:
+                newx , newy = x + dx , y + dy
+                if inbound(newx , newy) and grid[newx][newy] == 1:
                     grid[newx][newy] = 2
-                    total = max(total , time + 1)
-                    deq.append((newx,newy,time + 1))
-            
+                    total = max(total,time+1)
+                    deq.append((newx,newy,time+1))
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 1:
                     return -1
-
         return total
+
+            
+
+
+            
+
+
+    
