accc3a8a54cc2e799cccf9ce0ac9c73ff4e06ffa
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-25 09:46:31 +0300

    Time: 146 ms (56.85%), Space: 19.3 MB (97.6%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,30 +1,23 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        n , m = len(mat) , len(mat[0])
-        directions = [(1,0),(-1,0),(0,-1),(0,1)]
-        def inbound(r,c):
-            return 0 <= r < n and 0 <= c < m
-        
-        matrix = [[-1] * m for _ in range(n)]
         deq = deque()
+        n , m = len(mat) , len(mat[0])
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        def inbound(x,y):
+            return 0<=x<n and 0<=y<m
+
         for i in range(n):
             for j in range(m):
                 if mat[i][j] == 0:
-                    matrix[i][j] = 0
                     deq.append((i,j,0))
-        
+                else:
+                    mat[i][j] = -1
         while deq:
-            i , j , moves = deq.popleft()
+            x , y , dis = deq.popleft()
             for dx , dy in directions:
-                newx , newy = dx + i , dy + j
-                if inbound(newx,newy) and matrix[newx][newy] == -1:
-                    matrix[newx][newy] = moves + 1
-                    deq.append((newx,newy,moves+1))
-        return matrix
-                
-        
+                newx , newy = dx + x , dy + y
+                if inbound(newx,newy) and mat[newx][newy] == -1:
+                    mat[newx][newy] = dis + 1
+                    deq.append((newx,newy,dis+1))
+        return mat
 
-
-
-        
-        
