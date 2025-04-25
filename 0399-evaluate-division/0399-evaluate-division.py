6eff6fabdf3800f683682163db747d04152756f5
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-25 11:50:38 +0300

    Time: 0 ms (100%), Space: 17.8 MB (74.28%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,35 +1,29 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = defaultdict(list)
-        def build():
-            for i, [a , b] in enumerate(equations):
-                graph[a].append((b,values[i]))
-                graph[b].append((a,1/values[i]))
-        build()
-        # print(dict(graph))
-        
-        def dfs(start,end,visited,pro):
-            if start == end and graph[start] and graph[end] :
+        store = defaultdict(list)
+        count = 0
+        for a , b in equations:
+            store[a].append((b,values[count]))
+            store[b].append((a,1/values[count]))
+            count += 1
+
+        def dfs(start,end,vis,pro):
+            if start == end and store[start] and store[end]:
                 return pro
-            
-            for d , v in graph[start]:
-                # visited = visited.copy()
-                if d not in visited:
-                    visited.add(d)
-                    ans =  dfs(d,end,visited,pro*v)
-                    if ans != -1 :
+            for v , p in store[start]:
+                if v not in vis:
+                    vis.add(v)
+                    ans = dfs(v,end,vis,pro*p)
+                    if ans != -1:
                         return ans
-                   
             return -1
 
-            
-        ans = []
+        res = []
         for a , b in queries:
-            ans.append(dfs(a,b,set([a]),1))
-        return ans
+            res.append(dfs(a,b,set([a]),1))
+        return res
 
-
-
+            
 
 
 
