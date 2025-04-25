7a4c4628c4d5702bc99a31947e288ede36924b76
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-25 10:14:47 +0300

    Time: 253 ms (50.27%), Space: 20 MB (82.85%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,19 +3,25 @@ class Solution:
         directions = [(1,0),(-1,0),(0,1),(0,-1)]
         def inbound(r,c):
             return 0<=r<len(grid) and 0<=c<len(grid[0])
-        def dfs(r,c):
-            for dx , dy in directions:
-                newX , newY = dx + r , dy + c
-                if inbound(newX,newY) and grid[newX][newY] == '1':
-                    grid[newX][newY] = '0'
-                    dfs(newX,newY)
+        count = 0
 
-
-        island = 0
-        row , col = len(grid) , len(grid[0])
-        for i in range(row):
-            for j in range(col):
+        
+        def bfs(x,y):
+            deq = deque()
+            deq.append((x,y))
+            grid[x][y] = 0
+            while deq:
+                x , y = deq.popleft()
+                for dx , dy in directions:
+                    newx , newy = x + dx , y + dy
+                    if inbound(newx,newy) and grid[newx][newy] == '1':
+                        grid[newx][newy] = '0'
+                        deq.append((newx,newy))
+        
+        n , m = len(grid) , len(grid[0])
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j] == '1':
-                    island += 1
-                    dfs(i,j)
-        return island
+                    count += 1
+                    bfs(i,j)
+        return count
