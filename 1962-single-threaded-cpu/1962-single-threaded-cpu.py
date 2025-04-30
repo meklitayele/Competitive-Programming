b55133ff9e6afb60c297a16960cbfeb715bcaa28
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-30 11:35:06 +0300

    Time: 347 ms (59.2%), Space: 57.8 MB (65.13%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.py
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.py
@@ -1,23 +1,25 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         res = []
-        #sort based on arrival time , store the index too
+
+        #sort with arrival time and store index
         tasks = sorted([(t[0],t[1],idx) for idx , t in enumerate(tasks)])
         time = tasks[0][0]
+        store = [] #heap with processing time
         i = 0
-        store = []
-      
-
+        
+        #iterate till we cover all tasks
         while len(res) < len(tasks):
+            #push everytask whose arrival time is less or equal to the current time
             while i < len(tasks) and tasks[i][0] <= time:
-                heapq.heappush(store,(tasks[i][1],tasks[i][2]))
+                heapq.heappush(store,(tasks[i][1],tasks[i][2])) #proccesing and idx
                 i += 1
             if store:
-                times , idx = heapq.heappop(store)
-                time += times
+                pt , idx = heapq.heappop(store)
+                time += pt
                 res.append(idx)
-            elif i<len(tasks):
+            elif i < len(tasks):
                 time = tasks[i][0]
-
         return res
+
 
