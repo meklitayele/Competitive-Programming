d1980356999206bdc033740ba9005c71d668ac93
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-04-30 09:38:07 +0300

    Time: 7 ms (50.54%), Space: 21.3 MB (61.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,12 +2,13 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         store = defaultdict(int)
         for num in nums:
-            store[num] -= 1
+            store[num] += 1
         
         store = list(store.items())
         store = [(count,num) for (num,count) in store]
 
         heapq.heapify(store)
-        ans = heapq.nsmallest(k,store)
+        ans = heapq.nlargest(k,store)
+        print(ans)
         return [x[1] for x in ans]
         
