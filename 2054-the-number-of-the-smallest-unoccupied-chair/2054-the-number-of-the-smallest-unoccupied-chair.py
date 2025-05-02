e387533c10e78ed988b0820d85cdb80e71ec96b7
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-05-02 11:41:33 +0300

    Time: 115 ms (40.22%), Space: 22.6 MB (31.25%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.py
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.py
@@ -1,18 +1,21 @@
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
         n = len(times)
-        arrivals = [(times[i][0] , i) for i in range(n)]
-        arrivals.sort()
+        chairs = [i for i in range(n)]
+        heapq.heapify(chairs)
 
-        free = list(range(n))
-        heapq.heapify(free)
+        occupied = []
+        times = sorted([t[0],t[1],idx] for idx , t in enumerate(times))
+        for at , lt , idx in times:
+            while occupied and occupied[0][0] <= at:
+                heapq.heappush(chairs,heappop(occupied)[1])
+            ch =  heappop(chairs)
+            if idx == targetFriend:
+                return ch
+            heapq.heappush(occupied, [lt , ch])
+            
+        
 
-        leaving = []
-        for at , idx in arrivals:
-            while leaving and leaving[0][0] <= at:
-                heapq.heappush(free,heapq.heappop(leaving)[1])
-            chair = heapq.heappop(free)
-            if idx  == targetFriend:
-                return chair
-            heapq.heappush(leaving,(times[idx][1],chair))
-        return -1
+
+
+        
