5bf15e3e0c344bdcac3c11ca76cd14df84b8bd20
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-05-13 11:03:08 +0300

    Time: 828 ms (19.58%), Space: 80.5 MB (5.11%) - LeetHub

--- 1507-check-if-there-is-a-valid-path-in-a-grid/1507-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/1507-check-if-there-is-a-valid-path-in-a-grid.py
@@ -2,47 +2,45 @@ class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
         n , m = len(grid) , len(grid[0])
         parent = {i:i for i in range(n*m)}
-        
+
         def find(x):
             if parent[x] == x:
                 return x
             parent[x] = find(parent[x])
             return parent[x]
-
         def union(x,y):
             r1 , r2 = find(x) , find(y)
             if r1 != r2:
                 parent[r2] = r1
-                
-        direction = {
-            'left' : (0,-1),
-            'right' : (0,1),
-            'up': (-1,0),
-            'down': (1,0) 
+        opp = {
+            'left' : ' right',
+            'right' : 'left',
+            'up' : 'down',
+            'down' : 'up'
         }
         types = {
-            1: ['left','right'],
-            2: ['up','down'],
-            3: ['left','down'],
-            4: ['down','right'],
-            5: ['left','up'],
-            6: ['up','right']
+            1 : ['left','right'],
+            2 : ['up','down'],
+            3 : ['left','down'],
+            4 : ['down','right'],
+            5 : ['left','up'],
+            6 : ['up','right']
         }
-        opp = {
-            'up' : 'down',
-            'down' : 'up',
-            'left' : 'right',
-            'right' : 'left'
+        directions = {
+            'left' : (0,-1),
+            'right' : (0,1),
+            'up' : (-1,0),
+            'down' : (1,0)
         }
 
         def inbound(r,c):
-            return 0<=r<n and 0<=c<m
+            return 0 <= r < n and 0 <= c < m
 
         for r in range(n):
             for c in range(m):
                 cell = grid[r][c]
                 for d in types[cell]:
-                    dr , dc = direction[d]
+                    dr , dc = directions[d]
                     nr , nc = r + dr , c + dc
                     if inbound(nr,nc):
                         neigh = grid[nr][nc]
@@ -50,6 +48,6 @@ class Solution:
                             i1 = r * m + c
                             i2 = nr * m + nc
                             union(i1,i2)
-        return find(0) == find(m*n-1)
-
-                
+        return find(0) == find(n*m-1)
+        
+       
