4bf0a55fd8e98c857a98a44f8ab22ef84f5fd6a0
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-05-14 11:47:59 +0300

    Time: 27 ms (2.04%), Space: 18.1 MB (21.07%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,17 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # the number of possible subsets
         n = pow(2,len(nums))
         res = []
-        
-        # 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7
-        for num in range(n): 
+
+        for num in range(n):
             sub = []
-            for i in range(num): 
-                if num & (1 << i): 
+            for i in range(num):
+                if num & (1 << i):
                     sub.append(nums[i])
             res.append(sub)
-
         return res
 
 
-        
+       
+
+
