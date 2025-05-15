4c09949905a6bd8cab38b244d73a69a40b7b231e
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-05-15 12:55:42 +0300

    Time: 0 ms (100%), Space: 18.1 MB (13.1%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,13 +1,5 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        ans = x ^ y
-        count = 0
-        def calc(num):
-            nonlocal count
-            while num > 0:
-                if (num & 1):
-                    count += 1
-                num //= 2
-        calc(ans)
-        return count
+        num = x ^ y
+        return bin(num).count('1')
         
