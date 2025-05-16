6d009b39535ff63846d123dc6c2a244e44a86416
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-05-16 14:00:47 +0300

    Time: 0 ms (100%), Space: 17.6 MB (77.67%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,27 +1,6 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        if num == 0:
-            return 1
-        
-        ans = []
-        def calc(n):
-            while n > 0:
-                if (n & 1):
-                    ans.append('0')
-                else:
-                    ans.append('1')
-                n //= 2
-            return ans
-        answer = calc(num)
-        answer =''.join(answer)
-        s = len(answer)
-        
-        total = 0
-        for i in range(s):
-            total += pow(2,i) * int(answer[i])
-        return total
-
-       
-        
-
-
+        n = num.bit_length()
+        ans = (1 << n) - 1
+        print(ans)
+        return ans ^ num
