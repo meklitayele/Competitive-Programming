49f1a2759facb183d2d598fc5b3a5a8f9aaa0a08
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-05-16 14:28:54 +0300

    Time: 35 ms (19.31%), Space: 19.2 MB (65.79%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,31 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        #1. brute force approach
-        # store = defaultdict(int)
-        # for num in nums:
-        #     store[num] += 1
-        # for key , value in store.items():
-        #     if value == 1:
-        #         return key
-
-        #2. bit manipulation
-        #count unbalanced ones occ % 3
-        #this doesn't handle negative numbers
         ans = 0
+
         for i in range(32):
-            ones = 0
+            count = 0
             for num in nums:
-                if num < 0:
-                    num = num & (2**32-1)  #two's complement for negative numbers
-                ones += (num  >> i) & 1
-            ones %= 3
-            ans |= (ones << i)
+                if num < 0 :
+                    num = num & (2 **32-1)
+                if (num >> i) & 1:
+                    count += 1
+            ans |= (count % 3) << i
 
+        
         if ans >= 2**31:
-            ans -= 2**32
-
+            ans -= 2 ** 32
         return ans
-
-
 
         
