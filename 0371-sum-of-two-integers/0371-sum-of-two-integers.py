b65ba910a72864d4d6ae0728c7e3e7b6440e7986
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-05-16 14:35:39 +0300

    Time: 0 ms (100%), Space: 17.6 MB (95.63%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,18 +1,20 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
+        carry = 0
         ans = 0
         sums = 0
-        carry = 0
 
         for i in range(32):
-            num1 , num2 = (a >> i) & 1 , (b >> i) & 1
-            sums = num1 ^ num2 ^ carry
-            carry = (num1 & carry) | (num2 & carry) | (num1 & num2)
+            bitA = (a >> i) & 1
+            bitB = (b >> i) & 1
+            sums = bitA ^ bitB ^ carry
+            carry = (bitA & bitB) | (bitA & carry) | (bitB & carry)
             ans |= (sums << i)
-        
-        if ans > 2**31:
+
+        if ans >= 2 ** 31:
             ans -= 2 ** 32
 
         return ans
+
 
 
