8d78a008e2e4327f4909149b18061175014982de
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-05-16 09:45:49 +0300

    Time: 79 ms (92.4%), Space: 33.8 MB (14.1%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.py
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.py
@@ -1,26 +1,15 @@
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
-        # in prefix and always use ~0 or -1 
-        #we don't need prefix and
-        # ans = ~0
-        # arr = []
-        # for n in nums:
-        #     ans &= n
-        #     arr.append(ans)
-        # print(arr)
-
+        #we can't use prefix and array
+        #compute using sliding window
         left = 0
-        start = 0
         maxL = 0
+        start = 0 #to check if they share any common bit
+
         for right in range(len(nums)):
-            #while there's a shared bit move left pointer 
-            while (start & nums[right]) != 0:
+            while (start & nums[right]) != 0:  #move as long as i have a shared bit
                 start ^= nums[left]
                 left += 1
-            start |= nums[right]
-            maxL = max(maxL , right-left+1)
-        return (maxL)
-
-
-
-        
+            start |= nums[right]  #set it with nums[right]
+            maxL = max(maxL,right-left+1)
+        return maxL
