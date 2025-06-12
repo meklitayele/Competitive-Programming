8a81d549eb72451bc687f64261750c3533239bf8
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-06-12 11:51:35 +0300

    Time: 4 ms (79.76%), Space: 30.2 MB (22.22%) - LeetHub

--- 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.py
+++ 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.py
@@ -1,18 +1,9 @@
 class Solution:
     def xorBeauty(self, nums: List[int]) -> int:
-        #1.brute force
-        # values = 0
-        # for i in range(len(nums)):
-        #     for j in range(len(nums)):
-        #         for k in range(len(nums)):
-        #             values = values ^ ((w)
-        #             print(k," or ", j, " and ", i)     
-        # return values
-  
-        ans = 0
-        for num in nums:
-            ans ^= num
-        return ans
+        start = 0
+        for n in nums:
+            start ^= n
+        return start
 
        
 
