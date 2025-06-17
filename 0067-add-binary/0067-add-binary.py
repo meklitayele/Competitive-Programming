ecbd16a9d0f2fb181f6bfee4fbb9242a8ebb8f18
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-06-17 20:06:27 +0300

    Time: 3 ms (46.65%), Space: 18 MB (29.05%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,19 +1,22 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        newA = 0
-        c = 0
-        for i in range(len(a)-1,-1,-1):
-            newA += pow(2,c) * int(a[i])
-            c+= 1
-        print(newA)
+        n , m = len(a) , len(b)
+        c1 , c2 = 0 , 0
+        sumA , sumB = 0 , 0
+
+        for i in range(n-1,-1,-1):
+            sumA += int(a[i]) * pow(2,c1) 
+            c1 += 1
         
-        newB = 0
-        c = 0
-        for i in range(len(b)-1,-1,-1):
-            newB += pow(2,c) * int(b[i])
-            c += 1
+        for i in range(m-1,-1,-1):
+            sumB += int(b[i]) * pow(2,c2)
+            c2 += 1
+         
+        val = sumA + sumB
+        return ((str(bin(val)))[2:])
+
+
         
-        return str(bin(newA+newB)[2:])
 
     
 
