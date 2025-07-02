416b4c1aa9fa2954a72b7c5ac8dc0931c8c662b5
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-07-02 21:46:58 +0300

    Time: 257 ms (44%), Space: 20.3 MB (42.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
         def inbound(r,c):
-            return 0<=r<len(grid) and 0<=c<len(grid[0])
-        count = 0
-
+            return 0 <= r < len(grid) and 0 <= c < len(grid[0])
+        total = 0
         
         def bfs(x,y):
             deq = deque()
             deq.append((x,y))
-            grid[x][y] = 0
+            grid[x][y] = '0'
             while deq:
-                x , y = deq.popleft()
+                x , y  = deq.popleft()
                 for dx , dy in directions:
                     newx , newy = x + dx , y + dy
                     if inbound(newx,newy) and grid[newx][newy] == '1':
                         grid[newx][newy] = '0'
                         deq.append((newx,newy))
         
-        n , m = len(grid) , len(grid[0])
-        for i in range(n):
-            for j in range(m):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    count += 1
+                    total += 1
                     bfs(i,j)
-        return count
+        return total
+
+            
+            
