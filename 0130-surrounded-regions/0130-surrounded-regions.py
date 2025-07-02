e7ea7e4319f7070267877b9f73468fbe10036e8a
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-07-02 22:21:41 +0300

    Time: 7 ms (57.64%), Space: 21.5 MB (88.43%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,16 +3,27 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
         def inbound(r,c):
-            return 0<=r<len(board) and 0<=c<len(board[0])
-       
-        def dfs(row,col):
-            if inbound(row,col) and board[row][col] == 'O':
-                board[row][col] = 's'
+            return  0 <= r < len(board) and 0 <= c < len(board[0])
+        
+        #dfs accros all the edges 
+        #assign diff variable to 0 at the edges
+        def dfs(x,y):
+            if board[x][y] != 'O':
+                return 
+
+            deq = deque()
+            deq.append((x,y))
+            board[x][y] = 's'
+            while deq:
+                x , y = deq.popleft()
                 for dx , dy in directions:
-                    newX , newY = row + dx , col + dy
-                    dfs(newX,newY)
+                    newx , newy = dx + x , dy + y
+                    if  inbound(newx,newy) and board[newx][newy] == 'O':
+                        board[newx][newy] = 's'
+                        deq.append((newx,newy))
+
         for r in range(len(board)):
             dfs(r,0)
             dfs(r,len(board[0])-1)
@@ -20,17 +31,16 @@ class Solution:
         for c in range(len(board[0])):
             dfs(0,c)
             dfs(len(board)-1,c)
-
+        
         for i in range(len(board)):
-            for j in range(len(board[i])):
+            for j in range(len(board[0])):
                 if board[i][j] == 's':
                     board[i][j] = 'O'
                 else:
                     board[i][j] = 'X'
-                    
-       
-                    
-
-
-
         
+
+
+
+
+                    
