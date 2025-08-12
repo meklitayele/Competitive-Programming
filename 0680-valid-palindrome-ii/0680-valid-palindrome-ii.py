5163695c81a714b654447f7110b9ca15cd1da3f0
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-08-12 13:09:23 +0300

    Time: 31 ms (91.76%), Space: 18.2 MB (12.86%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,17 +1,17 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        #delete at mose one okay
-        left , right = 0 , len(s)-1
+        left ,  right = 0 , len(s)-1
         while left < right:
             if s[left] != s[right]:
-                if s[left:right][::-1] == s[left:right]:
+                if s[left:right] == s[left:right][::-1]:
                     return True
-                elif  s[left+1:right+1][::-1] == s[left+1:right+1]:
+                elif s[left+1:right+1] == s[left+1:right+1][::-1]:
                     return True
                 else:
                     return False
             left += 1
             right -= 1
         return True
-
+                    
+        
        
