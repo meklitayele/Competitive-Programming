8f0d8610c3b1b76c01d6753f41fe621cf5969e0e
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-08-20 21:10:14 +0300

    Time: 5 ms (65.54%), Space: 21.7 MB (72.26%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,44 +3,39 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
-        def inbound(r,c):
-            return  0 <= r < len(board) and 0 <= c < len(board[0])
-        
-        #dfs accros all the edges 
-        #assign diff variable to 0 at the edges
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        n , m = len(board) , len(board[0])
+
+        def inbound(x,y):
+            return 0 <= x < n and 0 <= y< m
+
         def dfs(x,y):
             if board[x][y] != 'O':
                 return 
-
-            deq = deque()
-            deq.append((x,y))
             board[x][y] = 's'
+            deq = deque([(x,y)])
             while deq:
                 x , y = deq.popleft()
                 for dx , dy in directions:
-                    newx , newy = dx + x , dy + y
-                    if  inbound(newx,newy) and board[newx][newy] == 'O':
+                    newx , newy = x + dx , y + dy
+                    if inbound(newx,newy) and board[newx][newy] == 'O':
                         board[newx][newy] = 's'
                         deq.append((newx,newy))
-
+                        dfs(newx,newy)
+        
         for r in range(len(board)):
             dfs(r,0)
-            dfs(r,len(board[0])-1)
-
+            dfs(r,m-1)
         for c in range(len(board[0])):
             dfs(0,c)
-            dfs(len(board)-1,c)
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
+            dfs(n-1,c)
+
+        for i in range(n):
+            for j in range(m):
                 if board[i][j] == 's':
                     board[i][j] = 'O'
                 else:
                     board[i][j] = 'X'
+        return board
+
         
-
-
-
-
-                    
