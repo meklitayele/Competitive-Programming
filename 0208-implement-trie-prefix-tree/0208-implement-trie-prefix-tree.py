6087bcc0c0d5b1e998eb1a130d015a13e36fb943
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2025-10-09 19:13:56 +0300

    Time: 104 ms (5.93%), Space: 36.8 MB (6.53%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,18 +1,18 @@
-class TrieNode:
+class Tri:
     def __init__(self):
         self.is_end = False
         self.children = [ None for _ in range(26) ]
 
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
+        self.root = Tri()
 
     def insert(self, word: str) -> None:
         curr = self.root
         for char in word:
             idx = ord(char) - ord('a')
             if not curr.children[idx] :
-                curr.children[idx] = TrieNode()
+                curr.children[idx] = Tri()
             curr = curr.children[idx]
         curr.is_end = True
         
